ae09f5eb95867496c8267d41ae26739b14a76672
udag <jeiljalnaga.lm@gmail.com>
2024-04-29 22:36:16 +0800

    paginator beta

--- blog/views.py
+++ blog/views.py
@@ -7,15 +7,19 @@ from .forms import PostForm
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from taggit.models import Tag
-
+from django.core.paginator import Paginator
 
 
 # views here
 
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date') 
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    paginator = Paginator(posts, 12)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'blog/post_list.html', {'page_obj': page_obj, 'posts':posts})
+
 
 def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
